2e59f149b70708298e552e9edd1a02215c3c82a6
derBeukatt <philburghardt@googlemail.com>
2014-04-02 13:38:14 +0200

    Menu entry for login

--- src/de/derBeukatt/anidb/gui/MainWindow.java
+++ src/de/derBeukatt/anidb/gui/MainWindow.java
@@ -3,6 +3,9 @@ package de.derBeukatt.anidb.gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class MainWindow {
 
@@ -38,6 +41,15 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		JMenu mnSettings = new JMenu("Settings");
+		menuBar.add(mnSettings);
+		
+		JMenuItem mntmLogin = new JMenuItem("Login");
+		mnSettings.add(mntmLogin);
 	}
 
 }
